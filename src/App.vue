75a675965ec5980f88b90ea6725bd2526fbf727f
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-26 16:20:34 +0300

    Header Yapımı(Sadece Eklemek)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
+  <div>
+    <app-header></app-header>
     <div class="container">
       <h1>Hello World!</h1>
     </div>
+  </div>
 </template>
 
 <script setup>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Deneme Header
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
