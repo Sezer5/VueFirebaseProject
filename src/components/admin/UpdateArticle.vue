c1dad525650eda60e683471585eaaeba818dfbcc
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-27 17:17:32 +0300

    Update işlemi bitti

--- src/components/admin/UpdateArticle.vue
+++ src/components/admin/UpdateArticle.vue
@@ -11,21 +11,22 @@
                     <label class="form-label">Yazı</label>
                     <textarea class="form-control" v-model="formData.description" rows="3"></textarea>
                 </div>
-                <button type="submit" class="btn btn-primary btn-lg">Ekle</button>
+                <button type="submit" class="btn btn-primary btn-lg">Güncelle</button>
             </form>
         </div>
     </div>
 </template>
 
 <script setup>
-    import { collection, addDoc } from "firebase/firestore"; 
+    // import { collection, addDoc } from "firebase/firestore"; 
+    import { doc, getDoc,updateDoc } from "firebase/firestore"; 
     import { reactive } from "vue";
     import {DB} from '@/firebase/configs.js';
-    import {useRouter} from 'vue-router';
+    import {useRouter,useRoute} from 'vue-router';
     import router from "@/router";
 
 
-
+    const route=useRoute();
 
     const routerMain=useRouter();
     const formData = reactive({
@@ -33,8 +34,26 @@
         description:''
     })
 
+    const docRef = doc(DB,"notes",route.params.id);
+
+    getDoc(docRef).then(snapShot => {
+        // console.log(snapShot.data());
+        formData.title = snapShot.data().title;
+        formData.description = snapShot.data().description;
+    }).catch(error=>{
+        console.log(error);
+    });
+
     const submitForm = async() =>{
-        
+            try {
+                const docRef = doc(DB,"notes",route.params.id);
+                await updateDoc(docRef,{...formData});
+                router.push('/');
+            } catch (error) {
+                console.log(error);
+            }
+             
+
     }
 
 </script>
