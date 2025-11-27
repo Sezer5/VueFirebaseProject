694fe6f0fa3eea65880655722e0ce6757b7c254f
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-27 11:05:35 +0300

    Kayıt olma formu oluşturuldu

--- src/components/users/Register.vue
+++ src/components/users/Register.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>Register</div>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <h1 class="text-center">Kayıt Formu</h1>
+            <form>
+                <div class="form-group mb-3">
+                    <label class="form-label">E-mail</label>
+                    <input type="email" class="form-control">
+                </div>
+                <div class="form-group mb-3">
+                    <label class="form-label">Şifre</label>
+                    <input type="password" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-primary btn-lg">Kaydol</button>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script setup>
