3a3fd37c37f1bc0d872d10e2e28a698e3ed99573
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-27 09:08:48 +0300

    Anasayfa Css Düzenlemeleri

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,21 @@
 <template>
-    <div>
-        Deneme Header
+    <div class="container">
+        <header class="d-flex py-3 border-bottom mb-5">
+            <a to="/" class="me-auto text-decoration-none text-dark">
+                <span class="fs-3">Sezer Ünalmış</span>
+            </a>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link"  to="/">Yazılarım</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" to="#">Çıkış Yap</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" to="#">Yazı Ekle</a>
+                </li>
+            </ul>
+        </header>
     </div>
 </template>
 
