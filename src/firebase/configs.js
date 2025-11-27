ef947e3c198068080b4a7c4998d7b36f235dbed3
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-27 11:31:00 +0300

    firebase eklentisi

---
+++ src/firebase/configs.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDc-mVAgGEr5fJscQfQf4cQvjPpBgrY9us",
+  authDomain: "fir-f551c.firebaseapp.com",
+  projectId: "fir-f551c",
+  storageBucket: "fir-f551c.firebasestorage.app",
+  messagingSenderId: "658996730114",
+  appId: "1:658996730114:web:dd04837342d73b16da4a1b",
+  measurementId: "G-4VHWRSE8QX"
+};
+
+initializeApp(firebaseConfig);
+
+const DB = getFirestore();
+
+export{DB}
