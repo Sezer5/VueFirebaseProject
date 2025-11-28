e4be7be436203cdac18f1f4ea70fef71a0842f49
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-28 17:24:20 +0300

    Firebase Projesi Bitişi

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,23 @@ import Home from '@/components/Home.vue'
 import AddArticle from '@/components/admin/AddArticle.vue'
 import UpdateArticle from '@/components/admin/UpdateArticle.vue'
 import Register from '@/components/users/Register.vue'
+import { AUTH } from '@/firebase/configs.js'
+
+const isAuth=(to,from)=>{
+    let user = AUTH.currentUser;
+    if(!user){
+      return '/users/register';
+    }
+    return true;
+}
+
+const isLoggedIn=(to,from)=>{
+    let user = AUTH.currentUser;
+    if(user){
+      return '/';
+    }
+    return true;
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,16 +33,20 @@ const router = createRouter({
       path: '/users/register',
       name: 'register',
       component: Register,
+      beforeEnter: isLoggedIn,
+      
     },
     {
       path: '/admin/addArticle',
       name: 'addarticle',
       component: AddArticle,
+      beforeEnter:isAuth
     },
     {
       path: '/admin/updateArticle/:id',
       name: 'updateArticle',
       component: UpdateArticle,
+      beforeEnter:isAuth
     }
     
   ],
